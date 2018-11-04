39547ac7cdf4a94a39f83f351bf218694f0cef50
BNM <Wowca111@mail.ru>
2018-11-04 19:35:27 +0300

    it works... somehow... Костыльный код, сделал флаги движения, которые меняются путем нажатия клавиш, затем если какой-то флаг активен, то вызывается функция из класса Bullet TODO: 1. нельзя выстрелить несколькими снарядами 2. почему то при выстреле и последующей смене направления, дальнейшая возможность стрелять по предыдущему направлению отсутствует.

--- games/main.py
+++ games/main.py
@@ -18,7 +18,7 @@ class Hero (pygame.sprite.Sprite):
         self.x = self.rect.x
         self.y = self.rect.y
 
-    def update(self,x, y):
+    def update(self, x, y):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
@@ -36,7 +36,7 @@ class Hero (pygame.sprite.Sprite):
                     if self.rect.y < H-190:
                         self.rect.y += 2
                         self.y = self.rect.y
-            if keys[pygame.K_DOWN] == 0 and keys[pygame.K_UP] == 0 and  self.rect.x > 90:
+            if keys[pygame.K_DOWN] == 0 and keys[pygame.K_UP] == 0 and self.rect.x > 90:
                 self.rect.x -= 3
                 self.x = self.rect.x
 
@@ -82,6 +82,12 @@ class Hero (pygame.sprite.Sprite):
                 self.rect_bullet.x += self.strike_speed
 
 
+MOVE_W = False
+MOVE_S = False
+MOVE_A = False
+MOVE_D = False
+
+
 class Bullet (Hero):
     def __init__(self, x, y, filename):
         pygame.sprite.Sprite.__init__(self)
@@ -91,9 +97,41 @@ class Bullet (Hero):
         self.rect_bullet = self.image.get_rect(center=(x, y))
         self.strike_speed = 5
 
-    def move(self):
-        if self.rect_bullet.y > 0:
+    def move_w(self):
+        if self.rect_bullet.y > 90:
             self.rect_bullet.y -= self.strike_speed
+        else:
+            self.kill()
+            global MOVE_W
+            MOVE_W = False
+    def move_s(self):
+        if self.rect_bullet.y < H-90:
+            self.rect_bullet.y += self.strike_speed
+        else:
+            self.kill()
+            global MOVE_S
+            MOVE_S = False
+    def move_a(self):
+        if self.rect_bullet.x > 90:
+            self.rect_bullet.x -= self.strike_speed
+        else:
+            self.kill()
+            global MOVE_A
+            MOVE_A = False
+
+    def move_d(self):
+        if self.rect_bullet.x < W-90:
+            self.rect_bullet.x += self.strike_speed
+        else:
+            self.kill()
+            global MOVE_D
+            MOVE_D = False
+
+
+class Monsters (Hero):
+    def __init__(self, x, y, filename):
+        pygame.sprite.Sprite.__init__(self)
+
 
 
 sc = pygame.display.set_mode((W, H))
@@ -114,14 +152,37 @@ while 1:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
+    keys = pygame.key.get_pressed()
 
     sc.blit(background_surf, background_rect)
     sc.blit(hero1.image, hero1.rect)
+    if keys[pygame.K_w]:
+        bullet1 = Bullet(hero1.x, hero1.y, 'pop.image.png')
+        MOVE_W = True
+    if keys[pygame.K_s]:
+        bullet1 = Bullet(hero1.x, hero1.y, 'pop.image.png')
+        MOVE_S = True
+    if keys[pygame.K_a]:
+        bullet1 = Bullet(hero1.x, hero1.y, 'pop.image.png')
+        MOVE_A = True
+    if keys[pygame.K_d]:
+
+        bullet1 = Bullet(hero1.x, hero1.y, 'pop.image.png')
+        MOVE_D = True
+    if MOVE_W == True:
+        bullet1.move_w()
+    if MOVE_S == True:
+        bullet1.move_s()
+    if MOVE_A == True:
+        bullet1.move_a()
+    if MOVE_D == True:
+        bullet1.move_d()
+
     sc.blit(bullet1.image, bullet1.rect_bullet )
     pygame.display.update()
     pygame.time.delay(20)
 
     hero1.update(W/2, H/2)
-    bullet1.strike()
+
     print(hero1.x, hero1.y)
 
